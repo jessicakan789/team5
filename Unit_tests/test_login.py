010cc74d1b3b2c9c589f9985c1053f15316c9f29
Emily Gibbons <emilyg250100@gmail.com>
2022-08-10 14:58:40 +0100

    Unit tests for login.py completed

--- Unit_tests/test_login.py
+++ Unit_tests/test_login.py
@@ -1,16 +1,99 @@
 import unittest
-from login import get_password
+from login import *
+import random
+import string
+
+# Generate random string for use in some unit-tests
+def get_random_string():
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(8))
 
 
-class PasswordTest(unittest.TestCase):
-    def test_user_exists(self):
-        expected = "a64eb7c6abe8010318b2f15cd7178699"  # the hash of password "L0u1sep00le@"
-        actual = get_password("LouisePoole1")
-        self.assertEqual(expected, actual)
 
-    def test_no_user(self):
-        result = get_password("HelloWorld")  # user does not exist
-        self.assertFalse(result)
+class NewUserClassTests(unittest.TestCase):
+
+    def test_is_unique(self):
+        while True:
+            random_user = get_random_string()
+            if random_user not in get_usernames():
+                user1 = NewUser(random_user, 'Dinosaur123!')
+                self.assertTrue(user1.is_unique())
+                break
+            else:
+                continue
+
+    def test_is_unique_exception(self):
+        existing_username = get_usernames()[0]
+        user1 = NewUser(existing_username,'Password123!')
+        with self.assertRaises(UsernameExistsError):
+            user1.is_unique()
+
+
+    def test_common_passwords_checker(self):
+        user1 = NewUser('test_user','Password123!')
+        with self.assertRaises(CommonPasswordError):
+            user1.common_passwords_checker()
+
+    def test_password_regex_too_short(self):
+        user1 = NewUser('test_user', 'Pp123!')
+        with self.assertRaises(RegexError):
+            user1.password_regex()
+
+    def test_password_regex_no_uppercase(self):
+        user1 = NewUser('test_user', 'password123!')
+        with self.assertRaises(RegexError):
+            user1.password_regex()
+
+    def test_password_regex_no_lowercase(self):
+        user1 = NewUser('test_user', 'PASSWORD123!')
+        with self.assertRaises(RegexError):
+            user1.password_regex()
+
+    def test_password_regex_no_numbers(self):
+        user1 = NewUser('test_user', 'Password!')
+        with self.assertRaises(RegexError):
+            user1.password_regex()
+
+    def test_password_regex_no_special_chars(self):
+        user1 = NewUser('test_user', 'Password1234')
+        with self.assertRaises(RegexError):
+            user1.password_regex()
+
+
+    def test_password_regex_valid(self):
+        user1 = NewUser('test_user','Password123!')
+        self.assertTrue(user1.password_regex())
+
+
+class ExistingUserClassTests(unittest.TestCase):
+
+    def test_login_success(self):
+        # This username and password needs to be in the database for test to pass
+        existing_username = 'emily2'
+        existing_password = 'Pumpkin123!'
+
+        user1 = ExistingUser(existing_username, existing_password)
+
+        self.assertTrue(user1.login())
+
+    def test_login_wrong_password(self):
+        existing_username = get_usernames()[0]
+        existing_password = get_password(existing_username)
+
+        incorrect_password = existing_password + get_random_string()
+
+        user1 = ExistingUser(existing_username, incorrect_password)
+
+        self.assertFalse(user1.login())
+
+    def test_login_wrong_username(self):
+        existing_username = get_usernames()[0]
+        existing_password = get_password(existing_username)
+
+        incorrect_username = existing_password + get_random_string()
+
+        user1 = ExistingUser(incorrect_username, existing_password)
+
+        self.assertFalse(user1.login())
 
 
 if __name__ == '__main__':
