cf7d84a5053f82b95f3d9eeba2af2e6961211fba
MaysaB <49612167+MaysaB@users.noreply.github.com>
2022-08-14 07:08:35 +0100

    access env's in os.

--- src/dbconfig.py
+++ src/dbconfig.py
@@ -5,3 +5,7 @@ RUN_ENV = os.environ.get('RUN_ENV')
 USER = "root"
 PASSWORD = '' if RUN_ENV == 'WEB' else input("Please enter your password")
 HOST = "localhost"
+
+# import operating system
+# from the os env, get the WEB_PASSWORD
+# if WEB in the RUN_ENV then run else ask for SQL password.
